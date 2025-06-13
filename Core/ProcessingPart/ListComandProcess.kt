b200b37665d4e0edeb2d69c9b9da4babb1cbb9cb
Márlon <mo14421890@gmail.com>
2025-06-12 22:35:59 -0300

    Uptade processcomand

--- Core/ProcessingPart/ListComandProcess.kt
+++ Core/ProcessingPart/ListComandProcess.kt
@@ -7,14 +7,28 @@ class ListComandProcess {
 
     fun processComand(): String{
         var comand: String = lista().ta()
-
         if (comand == "/Sad"){
             secret().secretSad()
         }else if(comand == "/Smile"){
             secret().secretSmile()
         }else if(comand == "/NumeroSecreto"){
             FoundNumber().SecretNumber()
+        }else if(comand =="/Box"){
+            secret().caixaSurpresa()
+        }else if(comand =="Amor"){
+            secret().coracao()
+        }else if(comand =="/Pinguin"){
+            secret().pinguim()
+        }else if(comand == "/Smiley"){
+            secret().smiley()
+        }else if(comand == "/Museu"){
+            secret().codigoIncrivel()
+        }else if(comand == "/Hacker"){
+            secret().hackerman()
+        }else{
+
         }
+
         return comand
 
     }
